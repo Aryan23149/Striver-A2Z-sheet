5758f9eb3cb18a3ef4dc97d99f237d5a7730364e
Aryan23149 <aryan23149@iiitd.ac.in>
2026-06-15 13:07:22 +0530

    updating on 15 june 2026

---
+++ geeks for geeks/online_code_submissions/subarray_sum_0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    public:
+      int findSubarray(vector<int> &arr) {
+          // code here.
+          
+          // int n =arr.size();
+          // int total=0;
+          // for(int i=0;i<n;i++){
+          //     int sum=0;
+          //     for(int j=i;j<n;j++){
+          //         sum+=arr[j];
+          //         if(sum==0)  total ++;
+          //     }
+          // }
+          // return total;
+          
+          // int n =arr.size();
+          // unordered_map<int,int> f;
+          // int sum=0;
+          // int count=0;
+          // f[0]=1;
+          // for(int i=0;i<n;i++){
+          //     sum+=arr[i];
+          //     count+=f[sum];
+          //     f[sum]++;
+          // }
+          // return count;
+      }
+  };
